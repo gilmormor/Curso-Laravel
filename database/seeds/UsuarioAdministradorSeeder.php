c1d69167b920ac8487b28ffa4e4707c9fe2d970c
Tutoriales virtuales <gilmormor@gmail.com>
2019-09-10 17:54:15 -0300

    Video  20 Pantalla de inicio de sesion login

---
+++ database/seeds/UsuarioAdministradorSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsuarioAdministradorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,11 @@
 //Route::view('permiso', 'permiso');
 //Route::get('admin/sistema/permisos','PermisoController@index')->name('permiso');
 
-Route::get('/','InicioController@index');
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+Route::get('/','InicioController@index')->name('inicio');
+Route::get('seguridad/login','Seguridad\LoginController@index')->name('login');
+Route::post('seguridad/login','Seguridad\LoginController@login')->name('login-post');
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'auth'], function () {
+    Route::get('', 'AdminController@index');
     Route::get('permiso', 'PermisoController@index')->name('permiso');
     Route::get('permiso/crear', 'PermisoController@crear')->name('crear_permiso');
     /*RUTAS DEL MENU*/
